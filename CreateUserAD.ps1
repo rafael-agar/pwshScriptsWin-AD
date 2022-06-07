2d379485c2a43b681d13c05d3b5912fb56a6ede9
Rafael Agar <94949315+rafael-agar@users.noreply.github.com>
2022-06-07 12:20:31 -0400

    Update CreateUserAD.ps1

--- CreateUserAD.ps1
+++ CreateUserAD.ps1
@@ -3,20 +3,35 @@ $lastname = Read-Host -Pormpt "...."
 $OU = Read-Host -Pormpt "...."
 $logon = $name.chars(0)
 
-New-ADUser `
- -SamAccountName "$logon$lastname"  `
- -UserPrincipalName "$logon$lastname" `
- -Name "$name" `
- -DisplayName "$name $lastname" `
- -GivenName "$name" `
- -SurName "$lastname" `
- -Company "..." `
- -Department "..." `
- -EmailAddress "$logon$lastname@domain.com"  `
- -Description "..." `
- -Country "..." `
- -Path "OU=$OU,DC=DOMAIN,DC=local" `
- -AccountPassword (ConvertTo-SecureString "P@ssword" -AsPlainText -force) `
- -Enabled $True `
- -PasswordNeverExpires $False `
- -ChangePasswordAtLogon $True -PassThru
+$result = @"
+Nmae: $name
+Last Name: $lastname
+Path: $OU
+username: $logon
+"@
+
+Write-Host "============================="
+Write-Host -ForegroundColor Green "Please check:"
+$result
+
+if ($confirm -eq 'yes') {
+    New-ADUser `
+    -SamAccountName "$logon$lastname"  `
+    -UserPrincipalName "$logon$lastname" `
+    -Name "$name" `
+    -DisplayName "$name $lastname" `
+    -GivenName "$name" `
+    -SurName "$lastname" `
+    -Company "..." `
+    -Department "..." `
+    -EmailAddress "$logon$lastname@domain.com"  `
+    -Description "..." `
+    -Country "..." `
+    -Path "OU=$OU,DC=DOMAIN,DC=local" `
+    -AccountPassword (ConvertTo-SecureString "P@ssword" -AsPlainText -force) `
+    -Enabled $True `
+    -PasswordNeverExpires $False `
+    -ChangePasswordAtLogon $True -PassThru
+} else {
+    Remove-Variable $name $lastname $logon $OU $result
+}
