7a06a02b834cb59a981ca1a154df3a394adf2d83
Rafael Agar <94949315+rafael-agar@users.noreply.github.com>
2022-06-07 11:56:04 -0400

    Update SetUp-FileServer.ps1

--- SetUp-FileServer.ps1
+++ SetUp-FileServer.ps1
@@ -1,10 +1,18 @@
 $domain = Read-Host -Pormpt "Type Domain Name: "
+Write-Host "You Domain Name is: $domain"
+$confirm = Read-Host 'Are you Sure You Want to Proceed? please type "yes", If Not Sure press "No" '
 
-New-NetFirewallRule -DisplayName "Allow ICMPv4" -Direction Inbound -Action Allow -Protocol ICMPv4
-New-NetFirewallRule -DisplayName "Allow ICMPv6" -Direction Inbound -Action Allow -Protocol ICMPv6
+if ($confirm -eq 'yes') {
+    #set Firewall Rulles
+    New-NetFirewallRule -DisplayName "Allow ICMPv4" -Direction Inbound -Action Allow -Protocol ICMPv4
+    New-NetFirewallRule -DisplayName "Allow ICMPv6" -Direction Inbound -Action Allow -Protocol ICMPv6
 
-#install ADDS
-Install-windowsfeature -name FS-FileServer
+    #install ADDS
+    Install-windowsfeature -name FS-FileServer
 
-#join the domain
-Add-Computer -DomainName $domain -credential (get-credential) -Restart
+    #join the domain
+    Add-Computer -DomainName $domain -credential (get-credential) -Restart
+}
+else {
+    Remove-Variable -Name $domain,$confirm
+}
